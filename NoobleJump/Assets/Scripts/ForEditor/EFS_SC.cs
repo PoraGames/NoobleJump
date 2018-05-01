7092a8b39b85487ef7a915bd541789423e2a4f02
ProRak <ProRak25@yandex.ru>
2018-05-01 18:25:59 +0500

    ++ EFS

---
+++ NoobleJump/Assets/Scripts/ForEditor/EFS_SC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Element for synchronize
+/// </summary>
+public class EFS_SC : MonoBehaviour
+{
+    public int id = 0;
+}
