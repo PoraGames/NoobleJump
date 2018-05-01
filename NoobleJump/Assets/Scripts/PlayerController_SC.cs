0a73e085c93e5a5ffef5dac87cf5346e1f3142bb
ProRak <ProRak25@yandex.ru>
2018-05-01 14:10:36 +0500

    Добавление макета основного персонажа + постоянный прыжок

---
+++ NoobleJump/Assets/Scripts/PlayerController_SC.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_SC : Unit_SC
+{
+    public float powerJump = 5f;
+    public float horisontalSpeed = 2f;
+
+    private Rigidbody2D rb;
+
+    #region Unity selection
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        InputReader();
+
+        if (GroundCheck())
+            Jump();
+    }
+    #endregion
+
+    /// <summary>
+    /// Вся обработка входных данных
+    /// </summary>
+    private void InputReader()
+    {
+
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, powerJump);
+    }
+}
---
+++ NoobleJump/Assets/Scripts/Unit_SC.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Все живое в игре
+/// </summary>
+public class Unit_SC : MonoBehaviour
+{
+    [Header("Unit")]
+    public int health = 10;
+
+    /// <summary>
+    /// Находится на земле
+    /// </summary>
+    public bool isGrounded = false;
+
+    /// <summary>
+    /// Точки для проверки контакта с землей
+    /// </summary>
+    public Transform[] groundCheckPoints;
+
+    public float groundCheckRadius = 0f;
+
+    public LayerMask whatIsGround;
+
+    /// <summary>
+    /// Уничтожить этого юнита
+    /// </summary>
+    public virtual void Kill()
+    {
+
+    }
+
+    protected bool GroundCheck()
+    {
+        foreach (Transform point in groundCheckPoints)
+        {
+            if (Physics2D.OverlapCircle(point.position, groundCheckRadius, whatIsGround))
+            {
+                isGrounded = true;
+                return isGrounded;
+            }
+        }
+
+        isGrounded = false;
+        return isGrounded;
+    }
+}
