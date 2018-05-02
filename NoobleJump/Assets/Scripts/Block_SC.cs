2d1a8e36bda301d038c31f04dbecdcf4ef7943c4
ProRak <ProRak25@yandex.ru>
2018-05-02 14:54:11 +0500

    ++ MapBuilder

---
+++ NoobleJump/Assets/Scripts/Block_SC.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// То из чего строится карта
+/// </summary>
+public class Block_SC : MonoBehaviour
+{
+    public Transform positionForCreatePoint;
+}
---
+++ NoobleJump/Assets/Scripts/MapBuilder_SC.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBuilder_SC : MonoBehaviour
+{
+    public Transform reactPoint;
+    public Transform createPoint;
+    public Transform playerPoint;
+
+    public Block_SC[] blocksForBuild;
+
+    private void Update()
+    {
+        if (CheckTheNeedToCreate())
+            CreateNewBlock();
+    }
+
+    bool CheckTheNeedToCreate()
+    {
+        return playerPoint.position.y >= reactPoint.position.y;
+    }
+
+    void CreateNewBlock()
+    {
+        // Выбор блока для создания
+        GameObject _generatedObj = GenerateBlockForCreating().gameObject;
+
+        // Создание
+        GameObject _createdObj = Instantiate(_generatedObj, createPoint.position, Quaternion.identity);
+        Block_SC _createdBlock = _createdObj.GetComponent<Block_SC>();
+
+        // Перемещение точек создания и ожидания
+        reactPoint.position += Vector3.up * (_createdBlock.positionForCreatePoint.position.y - createPoint.position.y);
+        createPoint.position = _createdBlock.positionForCreatePoint.position;
+    }
+
+    /// <summary>
+    /// Выбор блока для создания
+    /// <para> Все проверки идут внутри </para>
+    /// </summary>
+    /// <returns> Уже готовый к созданию блок </returns>
+    Block_SC GenerateBlockForCreating()
+    {
+        Block_SC _block = blocksForBuild[Random.Range(0, blocksForBuild.Length)];
+
+        // TODO: место для проверок
+
+        return _block;
+    }
+}
