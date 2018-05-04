eaf63dffaf7af6e5826d7f826bab5c7c586a7358
ProRak <ProRak25@yandex.ru>
2018-05-04 16:56:16 +0500

    Добавление Gizmoz в Object_SC

---
+++ NoobleJump/Assets/Scripts/ForEditor/GizmoSphere.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoSphere : MonoBehaviour
+{
+    public float size = 0f;
+    public Color color = Color.yellow;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, size);
+    }
+}
