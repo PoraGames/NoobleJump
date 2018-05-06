4b6152dc81843398f88870772fcd5fc321333944
ProRak <ProRak25@yandex.ru>
2018-05-06 22:14:13 +0500

    ++ Картинки шипов для Kill zone

---
+++ NoobleJump/Assets/Scripts/MapBuilding/VisualEffects_SC/RandemPictureOnCreate_SC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandemPictureOnCreate_SC : MonoBehaviour
+{
+    public Sprite[] pictures;
+
+	void Start ()
+	{
+        // Если есть SpriteRenderer,
+        // то меняем стандартную картинку на случайную из массива
+
+	    SpriteRenderer sr = GetComponent<SpriteRenderer>();
+	    if (sr)
+	        sr.sprite = pictures[Random.Range(0, pictures.Length)];
+	}
+}
