991f2d9531d1bfb9e7c2a50dc4766072c25254aa
ProRak <ProRak25@yandex.ru>
2018-05-09 13:01:24 +0500

    ++ Функционал паузы

---
+++ NoobleJump/Assets/Scripts/GameObjectExp_SC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Расширение класса GameObject
+/// </summary>
+public static class GameObjectExp_SC
+{
+    /// <summary> Вызов SetActive(activeState) у всех элементов </summary>
+    public static void SetActive(this GameObject[] gameObjects, bool activeState)
+    {
+        foreach (GameObject obj in gameObjects)
+        {
+            obj.SetActive(activeState);
+        }
+    }
+}
---
+++ NoobleJump/Assets/Scripts/Menu/PauseMenu_SC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu_SC : MonoBehaviour
+{
+    public GameObject[] pauseMenuOgjects;
+    public GameObject[] inGameMenuOgjects;
+
+    public void PauseGame()
+    {
+        pauseMenuOgjects.SetActive(true);
+        inGameMenuOgjects.SetActive(false);
+        Dispatcher_SC.Send(EventId.gamePaused, new EventInfo());
+        Time.timeScale = 0;
+    }
+
+    public void ContinueGame()
+    {
+        pauseMenuOgjects.SetActive(false);
+        inGameMenuOgjects.SetActive(true);
+        Dispatcher_SC.Send(EventId.gameContinue, new EventInfo());
+        Time.timeScale = 1;
+    }
+}
