f8522014680da5fe40c61fe9b73e185b4fe24405
ProRak <ProRak25@yandex.ru>
2018-05-12 21:28:35 +0500

    ++ макет RandomReplace

---
+++ NoobleJump/Assets/Scripts/MapBuilding/ObjectsGenerateControllers/RandomReplaceOnStart_SC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomReplaceOnStart_SC : MonoBehaviour
+{
+    public Transform[] generateAreaFrame;
+
+    private void OnDrawGizmos()
+    {
+        if (generateAreaFrame != null && generateAreaFrame.Length > 1)
+        {
+            foreach (Transform point1 in generateAreaFrame)
+            {
+                foreach (Transform point2 in generateAreaFrame)
+                {
+                    Gizmos.DrawLine(point1.position, point2.position);
+                }
+            }
+        }
+    }
+}
