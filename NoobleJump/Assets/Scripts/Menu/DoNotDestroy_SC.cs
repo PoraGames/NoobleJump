f3a03ca364a9ec12490bd95992c171631ca884a4
ProRak <ProRak25@yandex.ru>
2018-05-13 23:39:21 +0500

    ++ Асинхронная загрузка + сплеш арт

---
+++ NoobleJump/Assets/Scripts/Menu/DoNotDestroy_SC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoNotDestroy_SC : MonoBehaviour
+{
+    public GameObject[] immortaleObjects;
+
+    void Awake()
+    {
+        foreach (GameObject obj in immortaleObjects)
+        {
+            DontDestroyOnLoad(obj);
+        }
+    }
+}
--- NoobleJump/Assets/Scripts/Menu/SceneLoader_SC.cs
+++ NoobleJump/Assets/Scripts/Menu/SceneLoader_SC.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader_SC : MonoBehaviour
 {
+    public Image splashImage;
+    public float timeBeforeStartSceneLoading = 1f;
+    public float timeAfterSceneLoading = 1f;
+
+    private string needLevelName;
+    private AsyncOperation asyncOperation;
+
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -13,6 +22,41 @@ public class SceneLoader_SC : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        // Dispatcher_SC.Send(EventId.splashOn, new EventInfo());
+        needLevelName = sceneName;
+
+        StartCoroutine(LoadLevelWithAsync());
+    }
+
+    IEnumerator LoadLevelWithAsync()
+    {
+        float timer = 0f;
+        while (timer < timeBeforeStartSceneLoading)
+        {
+            timer += Time.deltaTime;
+            splashImage.color = new Color(1, 1, 1, timer / timeBeforeStartSceneLoading);
+
+            yield return null;
+        }
+
+        asyncOperation = SceneManager.LoadSceneAsync(needLevelName);
+        asyncOperation.allowSceneActivation = true;
+
+        while (!asyncOperation.isDone)
+        {
+            Debug.Log(asyncOperation.progress);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        timer = 0f;
+        while (timer < timeAfterSceneLoading)
+        {
+            Debug.Log("afterparty");
+            timer += Time.deltaTime;
+            splashImage.color = new Color(1, 1, 1, 1 - (timer / timeAfterSceneLoading));
+
+            yield return null;
+        }
     }
 }
