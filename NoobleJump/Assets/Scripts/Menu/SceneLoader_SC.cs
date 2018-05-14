0199e6b9df8843db59a71d735d936b54132a7d84
ProRak <ProRak25@yandex.ru>
2018-05-14 23:41:39 +0500

    ++ Прогресс бар + настройка плавной загрузки

--- NoobleJump/Assets/Scripts/Menu/SceneLoader_SC.cs
+++ NoobleJump/Assets/Scripts/Menu/SceneLoader_SC.cs
@@ -7,8 +7,13 @@ using UnityEngine.UI;
 public class SceneLoader_SC : MonoBehaviour
 {
     public Image splashImage;
+    public Slider progressBar;
+    [Header("Время появления сплеша")]
     public float timeBeforeStartSceneLoading = 1f;
+    [Header("Время исчезнавения сплеша")]
     public float timeAfterSceneLoading = 1f;
+    [Header("Обязательное время загрузки")]
+    public float minLoadinfTime = 2f;
 
     private string needLevelName;
     private AsyncOperation asyncOperation;
@@ -16,6 +21,13 @@ public class SceneLoader_SC : MonoBehaviour
 
     void Awake()
     {
+        // При уровне High наблюдаются фризы
+        // High = 50ms
+        // Normal = 10ms
+        // BelowNormal = 4ms
+        // Low = 2ms
+        Application.backgroundLoadingPriority = ThreadPriority.Normal;
+
         Dispatcher_SC.Subscribe(EventId.needLoadScene, OnNeedLoadScene);
         Dispatcher_SC.Subscribe(EventId.needReloadScene, OnNeedReloadScene);
     }
@@ -46,6 +58,7 @@ public class SceneLoader_SC : MonoBehaviour
         Time.timeScale = 1;
         loadingNow = true;
         needLevelName = sceneName;
+
         StartCoroutine(LoadLevelWithAsync());
     }
 
@@ -60,7 +73,7 @@ public class SceneLoader_SC : MonoBehaviour
     }
 
     /// <summary>
-    /// Асинхронная загрузка уровня, включает управление сплеш артом
+    /// Асинхронная загрузка уровня, включает управление сплеш артом и прогресс баром
     /// </summary>
     IEnumerator LoadLevelWithAsync()
     {
@@ -69,32 +82,40 @@ public class SceneLoader_SC : MonoBehaviour
         while (timer < timeBeforeStartSceneLoading)
         {
             timer += Time.deltaTime;
-            splashImage.color = new Color(1, 1, 0, timer / timeBeforeStartSceneLoading);
+            splashImage.color = new Color(1, 1, 1, timer / timeBeforeStartSceneLoading);// Постепенное появление сплеш арта
 
             yield return null;
         }
 
         // Загрузка
-        asyncOperation = SceneManager.LoadSceneAsync(needLevelName);
-        while (!asyncOperation.isDone)
+        progressBar.gameObject.SetActive(true);
+        progressBar.value = 0f; // Сбросить прогресс прогресс бара
+        asyncOperation = SceneManager.LoadSceneAsync(needLevelName); // Начать загружать на фоне
+        float fakeTimeEnd = Time.time + minLoadinfTime; // Для симуляции загрузки сложного уровня
+        while (!asyncOperation.isDone || Time.time < fakeTimeEnd)
         {
-            // TODO: Место для визуализации прогресса загрузки уровня
-            splashImage.color = new Color(1 - asyncOperation.progress, 1, 0, 1);
-            Debug.Log(asyncOperation.progress);
+            // Прогресс бар двигается по самому медленному процессу "загрузки"
+            float realProgress = asyncOperation.progress / 0.9f; // Реальный прогресс загрузки
+            float fakeProgress = (minLoadinfTime - (fakeTimeEnd - Time.time)) / minLoadinfTime; // Симуляция
+            if (fakeProgress < realProgress) // Берем наименьшее
+                realProgress = fakeProgress;
 
-            yield return new WaitForFixedUpdate();
+            progressBar.value = realProgress; // Двигаем прогресс бар
+
+            yield return null; // Ожидание до следующего Update;
         }
 
         // Картинка после загрузки
+        progressBar.gameObject.SetActive(false);
         timer = 0f;
         while (timer < timeAfterSceneLoading)
         {
             timer += Time.deltaTime;
-            splashImage.color = new Color(1, 1, 0, 1 - (timer / timeAfterSceneLoading));
+            splashImage.color = new Color(0.8f, 1, 0.8f, 1 - (timer / timeAfterSceneLoading));// Постепенное исчезание сплеш арта
 
             yield return null;
         }
 
-        loadingNow = false;
+        loadingNow = false;// Загрузка завершена
     }
 }
